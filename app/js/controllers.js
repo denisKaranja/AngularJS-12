5f4dcb883dd08b1e2376a128e4a0127c85e3855c
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-24 22:12:19 +0300

    Add REST and custom services
    Use REST() to get phone list rather than the

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,28 +1,22 @@
-'use strict';
+"use strict";
 
 /* Controllers */
-var phonecatControllers = angular.module('phonecatControllers', []);
+var phonecatControllers = angular.module("phonecatControllers", []);
 
-phonecatControllers.controller('PhoneListCtrl', ["$http", "$scope", function($http, $scope){
-  $http.get("phones/phones.json").success(function(data){
-    $scope.phones = data.splice(0, 10);
-  });
-
+phonecatControllers.controller("PhoneListCtrl", ["$scope", "Phone", function($scope, Phone) {
+  $scope.phones = Phone.query();
   $scope.orderProp = "age";
-
 }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
-    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
-      $scope.phone = data;
-      $scope.mainImageUrl = data.images[0];
-    });
+phonecatControllers.controller("PhoneDetailCtrl", ["$scope", "$routeParams", "Phone", function($scope, $routeParams, Phone) {
+  $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+    $scope.mainImageUrl = phone.images[0];
+  });
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    };
-  }]);
+  $scope.setImage = function(imageUrl) {
+    $scope.mainImageUrl = imageUrl;
+  }
+}]);
 
 
 
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,38 +3,46 @@
 
 describe('PhoneCat controllers', function() {
 
-describe('PhoneListCtrl', function(){
-  var scope, ctrl, $httpBackend;
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
 
-  // Load our app module definition before each test.
   beforeEach(module('phonecatApp'));
+  beforeEach(module('phonecatServices'));
 
-  // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-  // This allows us to inject a service but then attach it to a variable
-  // with the same name as the service in order to avoid a name conflict.
-  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('phones/phones.json').
-        respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-    scope = $rootScope.$new();
-    ctrl = $controller('PhoneListCtrl', {$scope: scope});
-  }));
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl, $httpBackend;
 
-  it('should create "phones" model with 2 phones fetched from xhr', function() {
-      expect(scope.phones).toBeUndefined();
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json').
+          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneListCtrl', {$scope: scope});
+    }));
+
+
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+      expect(scope.phones).toEqualData([]);
       $httpBackend.flush();
 
-      expect(scope.phones).toEqual([{name: 'Nexus S'},
-                                   {name: 'Motorola DROID'}]);
+      expect(scope.phones).toEqualData(
+          [{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
     });
 
-  it('should set the default value of orderProp model', function() {
+
+    it('should set the default value of orderProp model', function() {
       expect(scope.orderProp).toBe('age');
     });
+  });
 
 
-  //PhoneDetailCtrl unit test
   describe('PhoneDetailCtrl', function(){
     var scope, $httpBackend, ctrl,
         xyzPhoneData = function() {
@@ -56,16 +64,12 @@ describe('PhoneListCtrl', function(){
 
 
     it('should fetch phone detail', function() {
-      expect(scope.phone).toBeUndefined();
+      expect(scope.phone).toEqualData({});
       $httpBackend.flush();
 
-      expect(scope.phone).toEqual(xyzPhoneData());
+      expect(scope.phone).toEqualData(xyzPhoneData());
     });
   });
-
-  });
-
-
 });
 
 
