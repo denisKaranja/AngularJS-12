51ffae7bbf5ed2d0c25da3667aaf0dc996cdbf31
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-24 21:31:46 +0300

    Add a checkmark custom filter
    Create a custom filter called  that checks if a phone had infrared or not and marks with a tick or cross if not

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,9 @@
 'use strict';
 
 /* Filters */
+// name of the filter is `checkmark`
+angular.module('phonecatFilters', []).filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+});
