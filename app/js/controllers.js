caf04e3d78875d97c9d6111e877f4620fdfbd748
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-24 18:54:12 +0300

    Add unit tests for the dynamic phone lister

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,7 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', ["$http", "$scope", function($http, $scope){
   $http.get("phones/phones.json").success(function(data){
-    $scope.phones = data;
+    $scope.phones = data.splice(0, 10);
   });
 
   $scope.orderProp = "age";
