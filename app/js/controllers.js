77145d8b1eb4ab57fe39c5ac9f9d32789ac78023
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-24 20:47:21 +0300

    Add content to phone-detail template
    Create a view for a sinlge phone detail

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', ["$http", "$scope", function($http, $scope){
+phonecatControllers.controller('PhoneListCtrl', ["$http", "$scope", function($http, $scope){
   $http.get("phones/phones.json").success(function(data){
     $scope.phones = data.splice(0, 10);
   });
@@ -12,8 +12,10 @@ phonecatApp.controller('PhoneListCtrl', ["$http", "$scope", function($http, $sco
 
 }]);
 
-phonecatApp.controller("PhoneDetailCtrl", ["$scope", "$routeParams", function($scope, $routeParams){
-  $scope.phoneId = $routeParams.phoneId;
+phonecatControllers.controller("PhoneDetailCtrl", ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http){
+  $http.get("phones/" + $routeParams.phoneId + ".json").success(function(data){
+    $scope.phone = data;
+  });
 }]);
 
 
